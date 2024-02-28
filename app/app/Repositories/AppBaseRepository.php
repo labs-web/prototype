13035e9282f7c0b4a5894fcf2cf9761ddc7787aa
jalil betroji <jalil.betroji@gmail.com>
2024-02-28 13:13:08 +0100

    develop merged

--- app/app/Repositories/AppBaseRepository.php
+++ app/app/Repositories/AppBaseRepository.php
@@ -13,11 +13,10 @@ abstract class AppBaseRepository {
     public function paginatedData($perpage){
         return $this->model->paginate($perpage);
     }
-    public function update($Obj,$validatedData){
-        $dataToUpdate = $this->model->findOrFail($Obj->id);
-        $dataToUpdate->update($validatedData);
+    public function update($validatedData){
+        $this->model->update($validatedData);
     }
-    public function store($validatedData){
+    public function store(array $validatedData){
         return $this->model->create($validatedData);
     }
     public function destroy($Obj){
