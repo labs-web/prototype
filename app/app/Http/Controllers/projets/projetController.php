cee92b9da7597cfff33ec11a9b384d6c55522e91
jalil betroji <jalil.betroji@gmail.com>
2024-03-02 21:39:11 +0100

    project frontend started

--- app/app/Http/Controllers/projets/projetController.php
+++ app/app/Http/Controllers/projets/projetController.php
@@ -4,8 +4,68 @@ namespace App\Http\Controllers\projets;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\projets\projetRequest;
+use App\Repositories\projet\ProjetRepository;
 
 class projetController extends Controller
 {
-    //
+    protected $projectRepository;
+    public function __construct(ProjetRepository $projetRepository){
+        $this->projectRepository = $projetRepository;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
