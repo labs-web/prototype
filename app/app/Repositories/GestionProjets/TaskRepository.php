a9f8a5d89cf942ec1323656055f19edc8573269e
boukharSoufiane1998 <boukhar.soufiane.coding@gmail.com>
2024-03-18 10:48:08 +0000

    update init app

--- app/app/Repositories/GestionProjets/TaskRepository.php
+++ app/app/Repositories/GestionProjets/TaskRepository.php
@@ -4,32 +4,31 @@ namespace App\Repositories\GestionProjets;
 
 use App\Models\GestionProjets\Task;
 use App\Models\GestionProjets\Projet;
-use App\Repositories\BaseRepositorie;
+use App\Repositories\AppBaseRepository;
 use Illuminate\Database\Eloquent\Model;
 
-class TaskRepository extends BaseRepositorie {
+class TaskRepository extends AppBaseRepository {
     protected $model;
 
     public function __construct(Task $task){
         $this->model = $task;
     }
 
-    public function paginate(){
-        return $this->model->with('project')->paginate();
+    public function paginatedData($perPage = 4){
+        return $this->model->with('project')->paginate($perPage);
     }
 
-    public function searchData($searchableData, $perPage = 4)
+    public function searchData($searchableData, $id, $perPage = 4)
     {
-        return $this->model->where(function ($query) use ($searchableData) {
+        return $this->model->where(function ($query) use ($searchableData, $id) {
             $query->where('nom', 'like', '%' . $searchableData . '%')
-                ->orWhere('description', 'like', '%' . $searchableData . '%');
-        })->paginate($perPage);
+                  ->orWhere('description', 'like', '%' . $searchableData . '%');
+        })->where('project_id', $id)->paginate($perPage);
     }
-
+    
     public function filter()
     {
        return Projet::all();
     }
    
 }
-
