84ad17d680e32135efae9e2d824775c2dc835ae1
Hamid <hamidachaou379@gmail.com>
2024-03-18 21:51:13 +0000

    edit script link js

--- maquettes/view/layouts/script-link.php
+++ maquettes/view/layouts/script-link.php
@@ -1,32 +1,39 @@
    <!-- scripts -->
    <!-- jQuery -->
+   <script src="../../node_modules/admin-lte/plugins/jquery/jquery.min.js"></script>
    <!-- jQuery UI 1.11.4 -->
    <script src="../plugins/jquery-ui/jquery-ui.min.js"></script>
-   <script src="../../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.min.js"></script>
-
    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
    <script>
        $.widget.bridge('uibutton', $.ui.button)
    </script>
    <!-- Bootstrap 4 -->
-   <script src="node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-
+   <script src="../../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+   <!-- ChartJS -->
+   <script src="../../node_modules/admin-lte/plugins/chart.js/Chart.min.js"></script>
+   <!-- Sparkline -->
+   <script src="../../node_modules/admin-lte/plugins/sparklines/sparkline.js"></script>
+   <!-- JQVMap -->
+   <script src="../../node_modules/admin-lte/plugins/jqvmap/jquery.vmap.min.js"></script>
+   <script src="../../node_modules/admin-lte/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+   <!-- jQuery Knob Chart -->
+   <script src="../../node_modules/admin-lte/plugins/jquery-knob/jquery.knob.min.js"></script>
    <!-- daterangepicker -->
-   <script src="node_modules/admin-lte/plugins/moment/moment.min.js"></script>
+   <script src="../../node_modules/admin-lte/plugins/moment/moment.min.js"></script>
    <script src="plugins/daterangepicker/daterangepicker.js"></script>
    <!-- Tempusdominus Bootstrap 4 -->
-   <script src="node_modules/admin-lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+   <script src="../../node_modules/admin-lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
    <!-- Summernote -->
-   <script src="node_modules/admin-lte/plugins/summernote/summernote-bs4.min.js"></script>
-   
+   <script src="../../node_modules/admin-lte/plugins/summernote/summernote-bs4.min.js"></script>
+   <!-- overlayScrollbars -->
+   <script src="../../node_modules/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
    <!-- AdminLTE App -->
-   <script src="node_modules/admin-lte/dist/js/adminlte.js"></script>
-
-   <script src="/node_modules/admin-lte/plugins/jquery/jquery.min.js"></script>
-
-
- 
-   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+   <script src="../../node_modules/admin-lte/dist/js/adminlte.js"></script>
+   <!-- AdminLTE for demo purposes -->
+   <script src="../../node_modules/admin-lte/dist/js/demo.js"></script>
+   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+   <script src="../../node_modules/admin-lte/dist/js/pages/dashboard.js"></script>
+   <script src="../../node_modules/admin-lte/dist/js/validationForm.js"></script>
+   <!-- scripts -->
+   
   <!-- Include the Quill library -->
