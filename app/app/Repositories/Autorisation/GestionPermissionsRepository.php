567743695817eef88898e93247f8edf8ffdaf6d2
husseinbouik <husseinbouik5@gmail.com>
2024-03-19 14:39:14 +0000

    Add searchData method to GestionPermissionsRepository

--- app/app/Repositories/Autorisation/GestionPermissionsRepository.php
+++ app/app/Repositories/Autorisation/GestionPermissionsRepository.php
@@ -11,5 +11,11 @@ class GestionPermissionsRepository extends BaseRepositorie {
     public function __construct(AutorisationController $Controller){
         $this->model = $Controller;
     }
+    public function searchData($searchableData, $perPage = 4)
+    {
+        return $this->model->where(function ($query) use ($searchableData) {
+            $query->where('name', 'like', '%' . $searchableData . '%');
+        })->paginate($perPage);
+    }
 
 }
