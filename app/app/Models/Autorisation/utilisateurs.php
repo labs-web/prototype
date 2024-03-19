badfae5fb15b60e49e5f2892d0f5c082330a5234
AdnanBennasare <bennassar.adnan.solicode@gmail.com>
2024-03-19 12:39:29 +0000

    utilisateurs

--- app/app/Models/Autorisation/utilisateurs.php
+++ app/app/Models/Autorisation/utilisateurs.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Autorisation;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class utilisateurs extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -18,7 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'prenom',
+        'nom',
         'email',
         'password',
     ];
