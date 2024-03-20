a00975694bbdff9d6a905902fd4c931202858dd3
husseinbouik <husseinbouik5@gmail.com>
2024-03-20 09:30:54 +0000

    Add search functionality to GestionPermissionsRepository

--- app/app/Repositories/Autorisation/GestionPermissionsRepository.php
+++ app/app/Repositories/Autorisation/GestionPermissionsRepository.php
@@ -12,6 +12,12 @@ class GestionPermissionsRepository extends BaseRepositorie {
     public function __construct(AutorisationController $Controller){
         $this->model = $Controller;
     }
-
+    
+    public function search($searchableData, $perPage = 4)
+    {
+        return $this->model->where(function ($query) use ($searchableData) {
+            $query->where('name', 'like', '%' . $searchableData . '%');
+        })->paginate($perPage);
+    }
 
 }
