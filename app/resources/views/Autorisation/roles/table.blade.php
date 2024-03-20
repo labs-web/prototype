cf3ecd3651d4d2b26b9fccdc96c676878854ae06
Hamid Achaou <hamidAchaou379@gmail.com>
2024-03-20 14:59:18 +0000

    save

--- app/resources/views/Autorisation/roles/table.blade.php
+++ app/resources/views/Autorisation/roles/table.blade.php
@@ -11,7 +11,14 @@
             <td>{{$role->name}}</td>
             <td class="text-center">
                 <a href="{{ route('roles.edit', $role->id) }}" class="btn btn-sm btn-default"><i class="fa-solid fa-pen-to-square"></i></a>
-                <a href="{{route('roles.destroy', $role->id)}}" class="btn btn-sm btn-danger"><i class="fa-solid fa-trash"></i></a>
+                {{-- <a href="{{route('roles.destroy', $role->id)}}" class="btn btn-sm btn-danger"><i class="fa-solid fa-trash"></i></a> --}}
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-danger btn-sm"  data-toggle="modal" data-target="#deleteMod">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+  
+
+  
             </td>
         </tr>
         @empty
@@ -19,3 +26,5 @@
         @endforelse
     </tbody>
 </table>
+
+<x-modal-delete-role />
---
+++ app/resources/views/components/modal-delete-role.blade.php
@@ -0,0 +1,22 @@
+  <!-- Modal -->
+  <div class="modal fade" id="deleteMod" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="deleteModalLabel">Confirmation Modal</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          Are you sure you want to delete this item?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-danger">Delete</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
