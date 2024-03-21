871ad7b2e0c58b1dbca07bafb28f83821309df1c
husseinbouik <husseinbouik5@gmail.com>
2024-03-21 12:25:04 +0000

    Add permission downloadSeeder route

--- app/routes/Autorisation.php
+++ app/routes/Autorisation.php
@@ -14,15 +14,19 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'Autorisations'], function (
     Route::put('controllers/{controller}', [GestionControllersController::class, 'update'])->name('controllers.update');
     Route::delete('controllers/{controller}', [GestionControllersController::class, 'destroy'])->name('controllers.destroy');
     Route::post('/downloadSeeder', [GestionControllersController::class, 'downloadSeeder'])->name('controllers.download');
+    
     // Routes for managing permissions
     Route::prefix('permissions')->group(function () {
-        Route::get('/', [PermissionController::class, 'index'])->name('permission.list');
+        Route::get('/', [PermissionController::class, 'index'])->name('permission.index');
         Route::get('/create', [PermissionController::class, 'create'])->name('permission.create');
         Route::post('/', [PermissionController::class, 'store'])->name('permission.store');
         Route::get('/{permission}/edit', [PermissionController::class, 'edit'])->name('permission.edit');
         Route::put('/{permission}', [PermissionController::class, 'update'])->name('permission.update');
         Route::delete('/{permission}', [PermissionController::class, 'destroy'])->name('permission.destroy');
+        Route::post('/downloadSeeder', [PermissionController::class, 'downloadSeeder'])->name('permission.download');
     });
+
+    
 });
 
 
