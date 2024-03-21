f448547cd391abb2bdf0c15c4df0720b76f8e751
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2024-03-21 07:46:19 +0000

    Add French translation for permission messages

---
+++ app/lang/fr/Autorisation/Permision/message.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'add' => 'Ajouter',
+    'permissions' => 'Les permissions',
+    'edit' => 'Editer',
+    'Delete' => 'Supprimer',
+    'cancel' => 'Quitter',
+    'choix' => 'Choisir un projet',
+    'addPermission' => 'Ajouter une permission',
+    'editPermission' => 'Editer la permission',
+    'controller' => 'Controller',
+    'action' => 'Action',
+    'create' => 'Créer',
+    'update' => 'Mettre à jour',
+    'delete' => 'Supprimer',
+    'search' => 'Rechercher',
+    'searchPermission' => 'Rechercher une permission',
+    'success' => 'Succès',
+    'error' => 'Erreur',
+    'name' => 'Nom',
+    'back' => 'Retour',
+];
