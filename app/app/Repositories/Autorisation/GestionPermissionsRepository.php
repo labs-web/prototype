f45de253b288be63ece280e4c643fe294b8a2250
husseinbouik <husseinbouik5@gmail.com>
2024-03-21 12:50:29 +0000

    Refactor GestionPermissionsRepository

--- app/app/Repositories/Autorisation/GestionPermissionsRepository.php
+++ app/app/Repositories/Autorisation/GestionPermissionsRepository.php
@@ -2,9 +2,11 @@
 
 namespace App\Repositories\Autorisation;
 
-use App\Models\Autorisation\Permission ;
-use App\Repositories\BaseRepositorie;
-use Illuminate\Support\Facades\Artisan;
+use App\Models\Autorisation\Controller as AutorisationController;
+use App\Repositories\BaseRepositorie; 
+use Illuminate\Support\Facades\Artisan; 
+use ReflectionClass;
+use ReflectionMethod;
 
 class GestionPermissionsRepository extends BaseRepositorie {
     protected $model;
@@ -22,6 +24,32 @@ class GestionPermissionsRepository extends BaseRepositorie {
 
     public function filter()
     {
-       return Permission::all();
+       return AutorisationController::all();
     }
+
+function extractControllerPermissions(string $basePath): array
+{
+    $permissions = [];
+
+    $files = glob($basePath . '/**/*.php', GLOB_BRACE);
+
+    foreach ($files as $file) {
+        $className = basename($file, '.php');
+
+        if (strpos($className, 'Controller') !== false) {
+            require_once $file;
+            $reflection = new ReflectionClass($className);
+            $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+
+            foreach ($methods as $method) {
+                if ($method->getName() !== '__construct') {
+                    $permissions[] = $method->getName() . '-' . $className;
+                }
+            }
+        }
+    }
+
+    return $permissions;
+}
+
 }
