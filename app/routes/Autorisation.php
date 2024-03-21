a6078154fc49a72699f0608cc760678df8a1ddbd
husseinbouik <husseinbouik5@gmail.com>
2024-03-21 12:20:11 +0000

    Add routes for managing permissions in Autorisation.php

--- app/routes/Autorisation.php
+++ app/routes/Autorisation.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Autorisation\GestionControllersController;
+use App\Http\Controllers\Autorisation\PermissionController;
 
 Route::group(['middleware' => ['auth'], 'prefix' => 'Autorisations'], function () {
     // Routes for managing controllers
@@ -13,6 +14,17 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'Autorisations'], function (
     Route::put('controllers/{controller}', [GestionControllersController::class, 'update'])->name('controllers.update');
     Route::delete('controllers/{controller}', [GestionControllersController::class, 'destroy'])->name('controllers.destroy');
     Route::post('/downloadSeeder', [GestionControllersController::class, 'downloadSeeder'])->name('controllers.download');
+    // Routes for managing permissions
+    Route::prefix('permissions')->group(function () {
+        Route::get('/', [PermissionController::class, 'list'])->name('permissions.list');
+        Route::get('/create', [PermissionController::class, 'create'])->name('permissions.create');
+        Route::post('/', [PermissionController::class, 'store'])->name('permissions.store');
+        Route::get('/{permission}', [PermissionController::class, 'show'])->name('permissions.show');
+        Route::get('/{permission}/edit', [PermissionController::class, 'edit'])->name('permissions.edit');
+        Route::put('/{permission}', [PermissionController::class, 'update'])->name('permissions.update');
+        Route::delete('/{permission}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
+    });
 });
+
 
 Auth::routes();
