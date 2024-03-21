45d294fd289fe6300f5f1b71282b29dfc59c2d26
Hamid <hamidachaou379@gmail.com>
2024-03-21 23:07:14 +0000

    add routes

--- app/routes/Autorisation.php
+++ app/routes/Autorisation.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Autorisation\GestionControllersController;
+use App\Http\Controllers\Autorisation\RolesController;
 
 Route::group(['middleware' => ['auth'], 'prefix' => 'Autorisations'], function () {
     // Routes for managing controllers
@@ -13,6 +14,11 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'Autorisations'], function (
     Route::put('controllers/{controller}', [GestionControllersController::class, 'update'])->name('controllers.update');
     Route::delete('controllers/{controller}', [GestionControllersController::class, 'destroy'])->name('controllers.destroy');
     Route::post('/downloadSeeder', [GestionControllersController::class, 'downloadSeeder'])->name('controllers.download');
+
+    // Role
+    Route::resource('/roles', RolesController::class);
+    Route::get('/roles/export',[RolesController::class,'export'])->name('role.export');
+    Route::post('/roles/import',[RolesController::class,'import'])->name('roles.import');
 });
 
 Auth::routes();
