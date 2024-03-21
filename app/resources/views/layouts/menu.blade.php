eb99d6ab63d3f2646a524104d46421ab04363b31
amine.lamchatab <alamchatab@gmail.com>
2024-03-21 11:24:01 +0100

    save changes

--- app/resources/views/layouts/menu.blade.php
+++ app/resources/views/layouts/menu.blade.php
@@ -1,10 +1,5 @@
 <!-- need to remove -->
-<li class="nav-item">
-    <a href="{{ route('home') }}" class="nav-link {{ Request::is('home') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
-        <p>Home</p>
-    </a>
-</li>
+
 
 <li class="nav-item">
     <a href="{{ route('task.index') }}" class="nav-link {{ Request::is('task.index') ? 'active' : '' }}">
