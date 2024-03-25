3771955b2ceea05b7e24de169f60de78143eeeb5
jalil betroji <jalil.betroji@gmail.com>
2024-03-25 10:32:01 +0000

    catching error enhanced

--- app/resources/views/GestionProjets/projet/create.blade.php
+++ app/resources/views/GestionProjets/projet/create.blade.php
@@ -5,13 +5,23 @@
 
     <section class="content">
         <div class="container-fluid">
+            @if ($errors->has('project_exists'))
+            <div class="alert alert-danger">
+                {{ $errors->first('task_exists') }}
+            </div>
+        @else
+            @if ($errors->has('unexpected_error'))
+                <div class="alert alert-danger">
+                    {{ $errors->first('unexpected_error') }}
+                </div>
+            @endif
+        @endif
             <div class="row">
                 <div class="col-md-12">
 
                     <div class="card card-info">
                         <div class="card-header">
-                            <h3 class="card-title"> <i
-                                    class="nav-icon fas fa-table"></i>{{ __('GestionProjets/projet/message.addProject') }}
+                            <h3 class="card-title">{{ __('GestionProjets/projet/message.add') }}
                             </h3>
                         </div>
                         @if (@session('success'))
@@ -19,17 +29,7 @@
                                 <span class="font-medium text-success">{{ session('success') }}</span>
                             </div>
                         @endif
-                        @if ($errors->has('project_exists'))
-                            <div class="alert alert-danger">
-                                {{ $errors->first('task_exists') }}
-                            </div>
-                        @else
-                            @if ($errors->has('unexpected_error'))
-                                <div class="alert alert-danger">
-                                    {{ $errors->first('unexpected_error') }}
-                                </div>
-                            @endif
-                        @endif
+                       
                         <!-- Obtenir le formulaire -->
                         @include('GestionProjets.projet.fields')
                     </div>
