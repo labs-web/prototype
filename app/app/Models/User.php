61253368bcaffceb177500db3c4bd76a83fb25ab
AdnanBennasare <bennassar.adnan.solicode@gmail.com>
2024-03-25 14:07:44 +0000

    save

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -8,8 +8,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-
-
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -25,7 +23,6 @@ class User extends Authenticatable
         'email',
         'password',
     ];
-
 
     /**
      * The attributes that should be hidden for serialization.
