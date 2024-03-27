234c67df12b6db9b91ec6ced19fe62f7a0dc3fe7
imrane sarsri <imransarsi.ii@gmail.com>
2024-03-27 00:35:38 +0000

    fix problem to hover effect in menu

--- app/resources/views/layouts/menu.blade.php
+++ app/resources/views/layouts/menu.blade.php
@@ -1,33 +1,45 @@
 <!-- need to remove -->
+@php
+    $current_route = $_SERVER['REQUEST_URI'];
 
+@endphp
+{{-- @dd($current_route) --}}
+<li class="nav-item">
+    <a href="{{ route('home') }}" class="nav-link {{ Request::is('/') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-home"></i>
+        <p>
+            Accueil
+        </p>
+    </a>
+</li>
 <li class="nav-item">
     <a href="{{ route('projets.index') }}" class="nav-link {{ Request::is('projets') ? 'active' : '' }}">
         <i class="nav-icon fas fa-table"></i>
-        <p>Projets
+        <p>
+            Projets
         </p>
     </a>
 </li>
 <li class="nav-item">
     <a href="{{ route('task.index') }}" class="nav-link {{ Request::is('projets/tâches') ? 'active' : '' }}">
         <i class="nav-icon fas fa-tasks"></i>
-        <p>Tâches
+        <p>
+            Tâches
         </p>
     </a>
 </li>
 <li class="nav-item">
-    <a href="{{ route('utilisateurs.index') }}" class="nav-link {{ Request::is('utilisateurs') ? 'active' : '' }}">
+    <a href="{{ route('utilisateurs.index') }}"
+        class="nav-link {{ strpos($current_route, 'utilisateurs') !== false ? 'active' : '' }}">
         <i class="fa-solid fa-users pl-1 pr-1"></i>
-        <p>Utilisateurs
+        <p>
+            Utilisateurs
         </p>
     </a>
 </li>
 
-@php
-    $current_route = $_SERVER['REQUEST_URI'];
-@endphp
-
 <li class="nav-item has-treeview">
-    <a href="#" class="nav-link {{ strpos($current_route, 'Autorisation') !== false ? 'active' : '' }}">
+    <a href="#" class="nav-link {{ strpos($current_route, 'Autorisations') !== false ? 'active' : '' }}">
         <i class="nav-icon fas fa-user-lock"></i>
         <p>
             Autorisations
@@ -36,27 +48,29 @@
     </a>
     <ul class="nav nav-treeview">
         <li class="nav-item">
-            <a href="" class="nav-link {{ Request::routeIs('') ? 'active' : '' }}">
+            <a href=""
+                class="nav-link {{ strpos($current_route, 'Autorisation.index') !== false ? 'active' : '' }}">
                 <i class=" far fa-check-circle nav-icon"></i>
                 <p>Autorisation</p>
             </a>
         </li>
         <li class="nav-item">
-            <a href="#" class="nav-link {{ Request::routeIs('') ? 'active' : '' }}">
+            <a href="{{ route('roles.index') }}"
+                class="nav-link {{ strpos($current_route, 'roles') !== false ? 'active' : '' }}">
                 <i class="far fa-user-circle nav-icon"></i>
                 <p>Rôle</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('controllers.index') }}"
-                class="nav-link {{ Request::routeIs('controllers.index') ? 'active' : '' }}">
+                class="nav-link {{ strpos($current_route, 'controllers') !== false ? 'active' : '' }}">
                 <i class="fas fa-gamepad nav-icon"></i>
                 <p>Controllers</p>
             </a>
         </li>
         <li class="nav-item ">
             <a href="{{ route('actions.index') }}"
-                class="nav-link {{ Request::routeIs('actions.index') ? 'active' : '' }}">
+                class="nav-link {{ strpos($current_route, 'actions') !== false ? 'active' : '' }}">
                 <i class="fas fa-cogs nav-icon"></i>
                 <p>Actions</p>
             </a>
--- app/routes/web.php
+++ app/routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/login');
-});
+    return view('home');
+})->middleware('auth')->name('home');
 
 Auth::routes(['register' => false]);
