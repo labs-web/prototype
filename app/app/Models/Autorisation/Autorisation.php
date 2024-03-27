33737d27bd5f46ae8c11da0261226cb4fa6fbc79
AdnanBennasare <bennassar.adnan.solicode@gmail.com>
2024-03-27 14:52:36 +0000

    save

--- app/app/Models/Autorisation/Autorisation.php
+++ app/app/Models/Autorisation/Autorisation.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Autorisation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'role' ,
+        'controller',
+        'action'
+    ];
+
+
 }
