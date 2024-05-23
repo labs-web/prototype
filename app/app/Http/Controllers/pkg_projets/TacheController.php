b0f30fe75eca2da47ee695e5ad1b7dcc7855df84
amine.lamchatab <alamchatab@gmail.com>
2024-05-23 12:48:30 +0100

    save

--- app/app/Http/Controllers/pkg_projets/TacheController.php
+++ app/app/Http/Controllers/pkg_projets/TacheController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\pkg_projets;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\AppBaseController;
 use App\Repositories\pkg_projets\TacheRepository;
 
@@ -12,4 +13,11 @@ class TacheController extends AppBaseController
     {
         $this->tacheRepository = $tacheRepository;
     }
+
+    public function index(Request $request)
+    {
+        $tachesData = $this->tacheRepository->paginate();
+        return view('pkg_projets.kanban', compact('tachesData'));
+    }
 }
+    
