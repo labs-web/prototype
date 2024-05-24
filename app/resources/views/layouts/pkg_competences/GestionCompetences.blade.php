499c83c60f71d87cca4a9101b2c1dd8b65b636c3
husseinbouik <husseinbouik5@gmail.com>
2024-05-24 09:38:29 +0100

    Remove unused navigation links and menu items for Competences package

--- app/resources/views/layouts/pkg_competences/GestionCompetences.blade.php
+++ app/resources/views/layouts/pkg_competences/GestionCompetences.blade.php
@@ -6,10 +6,53 @@
             <i class="fas fa-angle-left right"></i>
         </p>
     </a>
-    @include('layouts.pkg_competences.Technologie')
-    @include('layouts.pkg_competences.Categorie')
-    @include('layouts.pkg_competences.Competence')
-    @include('layouts.pkg_competences.Niveau')
-
+    <ul class="nav nav-treeview">
+        <li class="nav-item ">
+            <a href="#" class="nav-link {{ Request::is('Technologies*') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-leaf"></i>
+                <p>
+                    Gestion des Technologies
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+            </a>
+        </li>
+    </ul>
+        <ul class="nav nav-treeview">
+        <li class="nav-item ">
+                <a href="#" class="nav-link {{ Request::is('categories*') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-leaf"></i>
+            <p>
+                Gestion des Categories
+                <i class="fas fa-angle-left right"></i>
+            </p>
+        </a>
+    
+    </li>
+    </ul>    <ul class="nav nav-treeview">
+        <li class="nav-item ">
+            <a href="#" class="nav-link {{ Request::is('Competences*') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-leaf"></i>
+                <p>
+                    Competences
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+            </a>
+    
+    
+        </li>
+    </ul>
+    
+    <ul class="nav nav-treeview">
+        <li class="nav-item ">
+            <a href="#" class="nav-link {{ Request::is('niveux*') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-leaf"></i>
+            <p>
+                Gestion des niveux
+                <i class="fas fa-angle-left right"></i>
+            </p>
+        </a>
+    
+    </li>
+    </ul>
 </li>
 
