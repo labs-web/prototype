dc52d9b1f1f515af861e5ffb20c5cc65c8c9dbb5
jalil betroji <jalil.betroji@gmail.com>
2024-05-24 15:50:04 +0100

    menu updated

--- app/routes/pkg_competences/NiveauxCompetences.php
+++ app/routes/pkg_competences/NiveauxCompetences.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\pkg_competences\NiveauxCompetencesController;
 
 // routes for Niveaux Competences management
 Route::middleware('auth')->group(function () {
-    Route::prefix('/')->group(function () {
+    Route::prefix('/competences')->group(function () {
         Route::resource('niveauxCompetences', NiveauxCompetencesController::class);
         Route::get('niveaux_Competences/export', [NiveauxCompetencesController::class, 'export'])->name('niveauxCompetences.export');
         Route::post('niveaux_Competences/import', [NiveauxCompetencesController::class, 'import'])->name('niveauxCompetences.import');
