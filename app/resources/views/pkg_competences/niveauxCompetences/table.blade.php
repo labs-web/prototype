b49e790c8be277e8685a3a1ad1b12677d75ea40f
jalil betroji <jalil.betroji@gmail.com>
2024-05-24 11:52:06 +0100

    js updated

--- app/resources/views/pkg_competences/niveauxCompetences/table.blade.php
+++ app/resources/views/pkg_competences/niveauxCompetences/table.blade.php
@@ -53,7 +53,7 @@
                     <i class="fas fa-file-download"></i>
                     {{ __('app.import') }}
                 </label>
-                <input type="file" id="upload" name="file" style="display:none;" onchange="submitForm()" />
+                <input type="file" id="upload" name="file" style="display:none;" />
             </form>
         @endcan
         @can('export-NiveauxCompetencesController')
