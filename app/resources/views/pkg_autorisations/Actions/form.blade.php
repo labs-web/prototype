5fb7d431ad03a3cbe0c618ad89efb3c41c2476aa
Safaâ Faiz <88689147+Safaa1faiz@users.noreply.github.com>
2024-05-24 13:20:33 +0100

    save

--- app/resources/views/pkg_autorisations/Actions/form.blade.php
+++ app/resources/views/pkg_autorisations/Actions/form.blade.php
@@ -1,17 +1,24 @@
-<form>
+<form action="{{ route('actions.store') }}" method="POST">
 <div class="form-group">
-    <label for="rActionName">Nom de Controller</label>
-    <select class="form-control" id="rActionName">
-        <option value="create-ProjectsController">ProjectsController</option>
-        <option value="create-TasksController">TasksController</option>
-        <option value="create-MemberController">MemberController</option>
-        <!-- Add more controller options as needed -->
+    <label for="exampleInputController">{{ __('pkg_autorisations/actions.Controller') }}</label>
+    <select name="controller_id" class="form-control" id="exampleInputController" required>
+        @if (isset($action))
+            <option value="{{ $action->controller->id }}">{{ $action->controller->nom }}</option>
+        @else
+            <option value="">{{ __('pkg_autorisations/actions.Controller') }}</option>
+        @endif
+        @foreach ($controllers as $item)
+            @if (!isset($action) || !$action->controller || $item->id !== $action->controller->id)
+                <option value="{{ $item->id }}">{{ $item->nom }}</option>
+            @endif
+        @endforeach
     </select>
 </div>
-
-    <div class="form-group">
-        <label for="rActionName">Nom de l'action</label>
-        <input type="text" class="form-control" id="rActionName" placeholder="Entrez le nom de l'action" value="create">
-    </div>
-    <button type="submit" class="btn btn-info">Ajouter</button>
+<div class="form-group">
+    <label for="nomAutorisationAction">{{ __('pkg_autorisations/actions.name') }}</label>
+    <input name="nom" type="text" class="form-control" id="nomAutorisationAction" placeholder="Entrer le nom"
+        value="{{ isset($action) ? $action->nom : null }}">
+               
+</div>
+<button type="submit" class="btn btn-info">Ajouter</button>
 </form>
