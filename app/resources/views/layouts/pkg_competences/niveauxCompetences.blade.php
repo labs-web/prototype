d734a46f57bf73689a5c7a248be87aa6c914d9e3
jalil betroji <jalil.betroji@gmail.com>
2024-05-28 21:47:05 +0100

    init merged

--- app/resources/views/layouts/pkg_competences/niveauxCompetences.blade.php
+++
@@ -1,6 +0,0 @@
-<li class="nav-item ">
-    <a href="{{ route('niveauxCompetences.index') }}" class="nav-link nav-link {{ Request::is('niveauxCompetences*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-table"></i>
-        <p>Niveaux competences</p>
-    </a>
-</li>
