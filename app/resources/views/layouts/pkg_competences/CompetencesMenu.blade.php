e778eb86115f2f624d610b7946e6d1a8890bb796
jalil betroji <jalil.betroji@gmail.com>
2024-05-29 15:06:49 +0100

    init merged

--- app/resources/views/layouts/pkg_competences/CompetencesMenu.blade.php
+++ app/resources/views/layouts/pkg_competences/CompetencesMenu.blade.php
@@ -8,13 +8,13 @@
     </a>
     <ul class="nav nav-treeview" style="{{ \Illuminate\Support\Str::contains(request()->url(), 'competences') ? 'display:block;' : '' }}">
         
-        <li class="nav-item">
+        {{-- <li class="nav-item">
             <a href="{{ route('technologies.index') }}"
                 class="nav-link {{ Route::is('technologies.index*') ? 'active' : '' }}">
                 <i class="nav-icon fas fa-table"></i>
                 <p>Technologies</p>
             </a>
-        </li>
+        </li> --}}
         <li class="nav-item">
             <a href="{{ route('niveauxCompetences.index') }}"
                 class="nav-link {{ Route::is('niveauxCompetences.index*') ? 'active' : '' }}">
@@ -23,7 +23,7 @@
             </a>
         </li>
        
-        <li class="nav-item">
+        {{-- <li class="nav-item">
             <a href="{{ route('competence.index') }}"
                 class="nav-link {{ Route::is('competence.index*') ? 'active' : '' }}">
                 <i class="nav-icon fas fa-table"></i>
@@ -36,6 +36,6 @@
                 <i class="nav-icon fas fa-table"></i>
                 <p>Categorie Technologie</p>
             </a>
-        </li>
+        </li> --}}
     </ul>
 </li>
