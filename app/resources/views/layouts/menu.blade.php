14806467202e2dafd8d8d9b8d9cf0c8aa8994b12
Hamid Achaou <hamidAchaou379@gmail.com>
2024-05-30 15:58:01 +0100

    fix

--- app/resources/views/layouts/menu.blade.php
+++ app/resources/views/layouts/menu.blade.php
@@ -7,7 +7,7 @@
     </a>
 </li>
 
-@include('layouts.GestionProjets.GestionProjetsMenu')
+@include('layouts.pkg_projets.GestionProjetsMenu')
 @include('layouts.pkg_competences.CompetencesMenu')
 @include('layouts.pkg_rh.pkg_rhMenu')
 @include('layouts.pkg_realisation_projet.RealisationMenu')
